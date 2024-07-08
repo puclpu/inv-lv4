da623f856a6038051ae9adaf79aaacf301467c39
kimchaerin <puclpu18@gmail.com>
2024-07-08 16:11:59 +0900

    🎉init commit

---
+++ sparta-lecture/src/main/java/com/sparta/spartalecture/SpartaLectureApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartalecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaLectureApplication.class, args);
+    }
+
+}
---
+++ sparta-lecture/src/test/java/com/sparta/spartalecture/SpartaLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartalecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
