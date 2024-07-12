addf6cce824d2d3db73d568db7810cfd0d841ba6
kimchaerin <puclpu18@gmail.com>
2024-07-12 11:38:16 +0900

    ✨회원 탈퇴 기능
    - 회원 탈퇴 api 구현
    - 회원과 연관된 데이터가 삭제될 수 있도록 양방향 관계, cascade 설정

--- sparta-lecture/src/main/java/com/sparta/spartalecture/user/service/UserService.java
+++ sparta-lecture/src/main/java/com/sparta/spartalecture/user/service/UserService.java
@@ -1,11 +1,16 @@
 package com.sparta.spartalecture.user.service;
 
+import com.sparta.spartalecture.global.exception.CustomException;
+import com.sparta.spartalecture.global.exception.CustomExceptionCode;
+import com.sparta.spartalecture.security.service.UserDetailsImpl;
 import com.sparta.spartalecture.user.dto.SignupRequestDto;
 import com.sparta.spartalecture.user.domain.User;
 import com.sparta.spartalecture.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +30,19 @@ public class UserService {
         // 사용자 등록
         User user = User.of(requestDto, password);
         userRepository.save(user);
+    }
+
+    public void withdraw(UserDetailsImpl userDetails) {
+
+        // 존재하는 회원인지 판별
+        User user = findUser(userDetails.getUser().getId());
+
+        // 회원 탈퇴
+        userRepository.delete(user);
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.MEMBER_NOT_FOUND));
     }
 }
